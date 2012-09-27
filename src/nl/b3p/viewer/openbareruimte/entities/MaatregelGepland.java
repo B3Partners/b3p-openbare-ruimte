6bd5a93aa8a1351894734087d1132dffc7715675
Roy Braam <braam.roy@gmail.com>
2012-09-27 14:35:01 +0000

    MaatregelEigenschappen worden nu ook opgeslagen als geplande maatregel wordt opgeslagen.

--- src/nl/b3p/viewer/openbareruimte/entities/MaatregelGepland.java
+++ src/nl/b3p/viewer/openbareruimte/entities/MaatregelGepland.java
@@ -6,6 +6,7 @@ package nl.b3p.viewer.openbareruimte.entities;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
@@ -25,7 +26,7 @@ public class MaatregelGepland {
     
     private Integer hoeveelheid;
     
-    @OneToMany(mappedBy="maatregel")
+    @OneToMany(mappedBy="maatregel", fetch= FetchType.LAZY, cascade={CascadeType.PERSIST, CascadeType.REMOVE, CascadeType.MERGE})
     private List<MaatregelEigenschap> eigenschappen;
 
     public void addEigenschap(MaatregelEigenschap me){
